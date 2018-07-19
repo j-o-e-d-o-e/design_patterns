e4003ae245ca470e9b670cfe8a453916313dc502
j-o-e-d-o-e <d_joe@gmx.net>
2018-07-19 18:02:37 +0200

    minor changes

--- src/facade/Client.java
+++ src/facade/Client.java
@@ -3,5 +3,10 @@ package facade;
 public class Client {
     public static void main(String[] args) {
         new Facade(new Guitar(), new Amp(), new Cable()).playGuitar("Take the Time");
+        try {
+            ((Guitar) Class.forName("facade.Guitar").newInstance()).on();
+        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
