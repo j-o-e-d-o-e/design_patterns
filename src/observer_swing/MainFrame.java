b947d25721c1ccd21bec7d83dc2badc6b198a2fb
j-o-e-d-o-e <d_joe@gmx.net>
2018-08-22 13:19:09 +0200

    minor changes

--- src/observer_swing/MainFrame.java
+++ src/observer_swing/MainFrame.java
@@ -9,7 +9,7 @@ class MainFrame extends JFrame {
         FormPanel formPanel = new FormPanel();
         formPanel.setFormListener(e -> {
             System.out.println(e.getName() + " " + e.getGender());
-            formPanel.clearData();
+            ((FormPanel)e.getSource()).clearData();
         });
         add(formPanel, BorderLayout.CENTER);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
