268faa7c0bdd6f78693e4ab62f9af2ab144fcc38
j-o-e-d-o-e <d_joe@gmx.net>
2018-08-23 17:22:57 +0200

    minor changes

--- src/factory/Factory.java
+++ src/factory/Factory.java
@@ -8,16 +8,16 @@ class Factory {
 
     // factory
     static FileSystemElement createNewElement(String path) {
-        LinkedList<String> where = new LinkedList<>(Arrays.asList(path.split("\\|"))); //alternatively, [|]
-        if (where.size() == 1) { // create root
+        LinkedList<String> where = new LinkedList<>(Arrays.asList(path.split("\\|"))); //alternatively, [|] as regex
+        if (where.size() == 1) { // create new root
             root = new Root(where.getFirst());
             return root;
         } else {
             where.removeFirst();
             FileSystemElement element;
-            if (path.contains(".")) // create file
+            if (path.contains(".")) // create new file
                 element = new File(where.getLast().substring(1));
-            else // create folder
+            else // create new folder
                 element = new Folder(where.getLast());
             where.removeLast();
             root.insert(element, where);
